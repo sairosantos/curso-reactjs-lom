f48a35e8a77d05de308dde43a70c51efbdbf8346
Sairo Santos <sairoraoni@gmail.com>
2023-06-03 16:20:15 -0300

    primeiro commit

--- src/App.js
+++ src/App.js
@@ -1,48 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
-import { Component } from 'react';
 
-class App extends Component {
-  state = {
-    name: 'Sairo Santos',
-    counter: 0
-  };
-
-  handlePClick = () => {
-    this.setState({ name: 'Júnior' });
-  }
-
-  handleAClick = (event) => {
-    event.preventDefault();
-    const { counter } = this.state;
-    this.setState({ counter: counter + 1 });
-  }
-
-  render() {
-    const { name, counter } = this.state;
-
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p onClick={this.handlePClick}>
-            {name}, {counter}.
-          </p>
-          <a onClick={this.handleAClick}
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Este é o link.
-          </a>
-        </header>
-      </div>
-    );
-  }
-}
-
-/*function App() {
+function App() {
   return (
     <div className="App">
       <header className="App-header">
@@ -61,6 +20,6 @@ class App extends Component {
       </header>
     </div>
   );
-}*/
+}
 
 export default App;
