b7ffd3d5c13677e64b033d7f0c11fb3390389d38
Sairo Santos <sairoraoni@gmail.com>
2023-06-04 17:43:04 -0300

    fetching e separação de componentes

--- src/App.js
+++ src/App.js
@@ -1,39 +1,30 @@
 import './App.css';
 import { Component } from 'react';
 
+import { Posts } from './components/Posts';
+import { loadPosts } from './utils/loadposts';
+
 class App extends Component {
   state = {
-    posts: [
-      {
-        id: 1,
-        title: 'O título 1',
-        body: 'O corpo 1'
-      },
-      {
-        id: 2,
-        title: 'O título 2',
-        body: 'O corpo 2'
-      },
-      {
-        id: 3,
-        title: 'O título 3',
-        body: 'O corpo 3'
-      }
-    ]
+    posts: []
   };
+
+  componentDidMount() {
+    this.loadPosts();
+  }
+
+  loadPosts = async () => {
+    const postsAndPhotos = await loadPosts();
+    this.setState({ posts: postsAndPhotos });
+  }
 
   render() {
     const { posts } = this.state;
 
     return (
-      <div className="App">
-        {posts.map(post => (
-          <div key={post.id}>
-            <h1>{post.title}</h1>
-            <p>{post.body}</p>
-          </div>
-        ))}
-      </div>
+      <section className="container">
+        <Posts posts={posts} />
+      </section>
     );
   }
 }
